0b1c475d6fd88464785bc427e3b44f7b64184917
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2019-10-04 15:38:18 -0400

    Add files via upload

--- sock352.py
+++ sock352.py
@@ -4,11 +4,43 @@ import socket as syssock
 import struct
 import sys
 
+
+MAX_PACKET_SIZE = 64000
+ 
+
+#creating a packet "struct"
+class packet:
+    def __init__(self,version,flags,header_len,sequence_no,ack_no,window,payload_len):     #initialize te packet
+        self.version = 0x1 #should always be 0x1
+        self.flags = flags
+        self.opt_ptr = 0
+        self.protocol = 0
+        self.header_len = header_len
+        self.checksum = 0
+        self.soure_port = 0
+        self.dest_port = 0
+        self.sequence_no = sequence_no
+        self.ack_no = ack_no
+        self.window = 0
+        self.payload_len = payload_len
+        return
+    def pack(self): #method to pack
+        return
+    def unpack(self): #method to unpack
+        return
+    
+    
+
+
+
 # these functions are global to the class and
 # define the UDP ports all messages are sent
 # and received from
 
 def init(UDPportTx,UDPportRx):   # initialize your UDP socket here 
+    #global port nums
+    global portTx
+    global portRx
     pass 
     
 class socket:
